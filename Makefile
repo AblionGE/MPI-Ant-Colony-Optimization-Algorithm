39b2cb876079a8d962516cee2e3a9fab9f8cb384
Marc Schaer <marc110191@gmail.com>
2015-12-04 11:30:59 +0100

    add generator of random numbers

--- Makefile
+++ Makefile
@@ -5,13 +5,19 @@ LDFLAGS			= -lm
 LDFLAGS_MPI	= $(LDFLAGS)
 
 EXEC_MAP		= generate_map
+EXEC_RAND		= generate_random_numbers
 
-all: map
+all: map random
 
 map:
 	$(CC) $(CFLAGS) generate_map.cpp
 	$(CC) $(LDFLAGS) generate_map.o -o $(EXEC_MAP)
 
+random:
+	$(CC) $(CFLAGS) generate_random_numbers.cpp
+	$(CC) $(LDFLAGS) generate_random_numbers.o -o $(EXEC_RAND)
+
 clean:
 	rm -f *.o $(EXEC_MAP)
+	rm -f *.o $(EXEC_RAND)
 
---
+++ generate_random_numbers.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+
+
+int main(int argc, char* argv[]) {
+
+  if (argc != 3) {
+    printf("use : %s fileName numberOfNumbers\n", argv[0]);
+    return -1;
+  }
+
+  std::ofstream outfile (argv[1]);
+  int size = atoi(argv[2]);
+  int* array;
+  int i;
+
+  // Allocation of map
+  array = (int*) malloc(size*sizeof(int));
+
+  for (i = 0; i < size; i++) {
+    array[i] = rand();
+  }
+
+  // Add the size as file first line
+  outfile << size << std::endl;
+
+  // Then copy all the matrix
+  for (i = 0; i < size; i++) {
+    outfile << array[i] << " ";
+  }
+  outfile << std::endl;
+
+  outfile.close();
+
+  // deallocate the pointers
+  free(array);
+
+  return 0;
+}
