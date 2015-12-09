7a5aab28c2ab16df25b64c83ea29d44de9d1f494
Marc Schaer <marc110191@gmail.com>
2015-12-09 15:40:19 +0100

    fix problem in plotRelativeSpeedup

--- parallel/plotRelativeSpeedup.sh
+++ parallel/plotRelativeSpeedup.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
 #file="results.txt"
-outputfile="output"
+outputfilename="output.txt"
 optimalfile="optimal.txt"
 graphfile="graph2.pdf"
 
 for file in *.out; do
   N=2
+  nodes=$(head -n 1 $file | awk -v N=$N '{print $N}')
   if [ 1 -eq $nodes ]; then
     lastLine=$(tail -1 $file)
     TRef=$(echo $lastLine | awk -v N=$N '{print $N}')
   fi
 done
 
-for file in *.out; do
-	# Delete old output file
-	outputfilename=$(echo $outputfile)
-	if [ -e $outputfilename ]; then
-    rm $outputfilename
-  fi
+# Delete old output file
+if [ -e $outputfilename ]; then
+  rm $outputfilename
+fi
 
-	# Take the second number of a line
-	N=2
-	nodes=$(head -n 1 $file | awk -v N=$N '{print $N}')
+for file in *.out; do
+
+  # Take the second number of a line
+  N=2
+  nodes=$(head -n 1 $file | awk -v N=$N '{print $N}')
   lastLine=$(tail -1 $file)
   T=$(echo $lastLine | awk -v N=$N '{print $N}')
+  echo $T
 
-	echo "$nodes $(echo $TRef $T | awk '{print $1 / $2}')" >> $outputfilename
+  echo "$nodes $(echo $TRef $T | awk '{print $1 / $2}')" >> $outputfilename
 
 done
 
-# Optimal Speedup
-rm $optimalfile
-for i in $(seq $lines); do
-	echo "$i $i" >> $optimalfile
-done
+## Optimal Speedup
+#rm $optimalfile
+#for i in $(seq $lines); do
+#	echo "$i $i" >> $optimalfile
+#done
 
 
 #	for i in $(seq $lines); do
@@ -59,7 +61,7 @@ done
 #set ylabel "Relative Speedup" 			# Label of y axis
 #set xlabel "Number of Machines"	# Label of x axis
 #plot '$resfile1' title "Speedup (local thread in Process)" with linespoints pointtype 5, \
-#'$resfile2' title "Speedup (Split-Merge coop)" with linespoints pointtype 3, \
-#'$resfile3' title "Speedup (all together)" with linespoints pointtype 4, \
-#'$optimal' title "Optimal Speedup" with linespoints pointtype 6
+  #'$resfile2' title "Speedup (Split-Merge coop)" with linespoints pointtype 3, \
+  #'$resfile3' title "Speedup (all together)" with linespoints pointtype 4, \
+  #'$optimal' title "Optimal Speedup" with linespoints pointtype 6
 #EOF
