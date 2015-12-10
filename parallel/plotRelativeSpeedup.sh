610fcddf24d024800fa20ce4a5d7f991599e4ba0
Marc Schaer <marc110191@gmail.com>
2015-12-10 09:16:26 +0100

    script for plotting relative speedup done

--- parallel/plotRelativeSpeedup.sh
+++ parallel/plotRelativeSpeedup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#file="results.txt"
 outputfilename="output.txt"
 optimalfile="optimal.txt"
-graphfile="graph2.pdf"
 
 for file in *.out; do
   N=2
@@ -25,43 +23,27 @@ for file in *.out; do
   nodes=$(head -n 1 $file | awk -v N=$N '{print $N}')
   lastLine=$(tail -1 $file)
   T=$(echo $lastLine | awk -v N=$N '{print $N}')
-  echo $T
-
   echo "$nodes $(echo $TRef $T | awk '{print $1 / $2}')" >> $outputfilename
-
 done
 
 ## Optimal Speedup
-#rm $optimalfile
-#for i in $(seq $lines); do
-#	echo "$i $i" >> $optimalfile
-#done
+if [ -e $optimalfile ]; then
+  rm $optimalfile
+fi
 
+lines=$(cat $outputfilename | wc -l)
+N=1
+for i in $(seq $lines); do
+  k=$(head -n $i $outputfilename | tail -n 1 | awk -v N=$N '{print $N}')
+	echo "$k $k" >> $optimalfile
+done
 
-#	for i in $(seq $lines); do
-#		Tnew=$(sed -n "${i}p" $file | awk -v N=$N '{print $N}')
-#		echo "$i $(echo $Told $Tnew | awk '{print $1 / $2}')" >> $outputfilename
-#	done
-#done
-## Optimal Speedup
-#rm $optimalfile
-#for i in $(seq $lines); do
-#	echo "$i $i" >> $optimalfile
-#done
-#
-#resfile1="outputresults2.txt" # Name of result file
-#resfile2="outputresults3.txt"
-#resfile3="outputresults4.txt"
-#optimal=$optimalfile
 #plot=$graphfile # Name of file containing plot
-#
-#gnuplot <<EOF
-#set terminal pdf 				# Output format (gif, png or jpeg also work)
-#set output "relativeSpeedup.pdf"				# Name of output file
-#set ylabel "Relative Speedup" 			# Label of y axis
-#set xlabel "Number of Machines"	# Label of x axis
-#plot '$resfile1' title "Speedup (local thread in Process)" with linespoints pointtype 5, \
-  #'$resfile2' title "Speedup (Split-Merge coop)" with linespoints pointtype 3, \
-  #'$resfile3' title "Speedup (all together)" with linespoints pointtype 4, \
-  #'$optimal' title "Optimal Speedup" with linespoints pointtype 6
-#EOF
+gnuplot <<EOF
+set terminal pdf 				# Output format (gif, png or jpeg also work)
+set output "relativeSpeedup.pdf"				# Name of output file
+set ylabel "Relative Speedup" 			# Label of y axis
+set xlabel "Number of Machines"	# Label of x axis
+plot '$outputfilename' title "Speedup (local thread in Process)" with linespoints pointtype 5, \
+'$optimalfile' title "Optimal Speedup" with linespoints pointtype 6
+EOF
