9657288b2fb5e062caf72eb46144c89a54382f23
Marc Schaer <marc110191@gmail.com>
2015-12-15 10:22:17 +0100

    fix compare_result

--- compare_results.sh
+++ compare_results.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 MAX_NODES=0
 OUTPUT_PARALLEL1="comparison1.txt"
@@ -28,22 +28,26 @@ do
     if [ "$temp" != "" ]; then
       N=$(head -n 1 $results | wc -w)
       INDEX=$(head -n 1 $results | awk -v N=$N '{print $N}')
-      SERIALP1[$INDEX]=0
-      P1_SERIAL[$INDEX]=0
+      if [ "$SERIAL_P1[$INDEX]" -eq "" ]; then
+        SERIAL_P1[$INDEX]=0
+      fi
+      if [ "$P1_SERIAL[$INDEX]" -eq "" ]; then
+        P1_SERIAL[$INDEX]=0
+      fi
       if [ "$INDEX" -gt "$MAX_NODES" ]; then
         MAX_NODES=$INDEX
       fi
       N=$(head -n 6 $results | tail -n 1 | wc -w)
       PARALLEL1COST[$INDEX]=$(head -n 6 $results | tail -n 1 | awk -v N=$N '{print $N}')
       if [ "$PARALLEL1COST[$INDEX]" -gt "$SERIALCOST" ]; then
-        SERIAL_P1[$INDEX]=$SERIAL_P1[$INDEX] + 1
+        SERIAL_P1[$INDEX]=$(echo "$SERIAL_P1[$INDEX] + 1" | bc)
       elif [ "$PARALLEL1COST[$INDEX]" -lt "$SERIALCOST" ]; then
-        P1_SERIAL[$INDEX]=$P1_SERIAL[$INDEX] + 1
+        P1_SERIAL[$INDEX]=$(echo "$P1_SERIAL[$INDEX] + 1" | bc)
       fi
     fi
   done
   cd ..
-  
+
   cd parallel2 
   for results in $(ls | grep slurm)
   do
@@ -51,17 +55,21 @@ do
     if [ "$temp" != "" ]; then
       N=$(head -n 1 $results | wc -w)
       INDEX=$(head -n 1 $results | awk -v N=$N '{print $N}')
-      SERIAL_P2[$INDEX]=0
-      P2_SERIAL[$INDEX]=0
+      if [ "$SERIAL_P2[$INDEX]" -eq "" ]; then
+        SERIAL_P2[$INDEX]=0
+      fi
+      if [ "$P2_SERIAL[$INDEX]" -eq "" ]; then
+        P2_SERIAL[$INDEX]=0
+      fi
       if [ $INDEX -gt $MAX_NODES ]; then
         MAX_NODES=$INDEX
       fi
       N=$(head -n 6 $results | tail -n 1 | wc -w)
       PARALLEL2COST[$INDEX]=$(head -n 6 $results | tail -n 1 | awk -v N=$N '{print $N}')
       if [ "$PARALLEL2COST[$INDEX]" -gt "$SERIALCOST" ]; then
-        SERIAL_P2[$INDEX]=$SERIAL_P2[$INDEX] + 1
+        SERIAL_P2[$INDEX]=$(echo "$SERIAL_P2[$INDEX] + 1" | bc)
       elif [ "$PARALLEL2COST[$INDEX]" -lt "$SERIALCOST" ]; then
-        P2_SERIAL[$INDEX]=$P2_SERIAL[$INDEX] + 1
+        P2_SERIAL[$INDEX]=$(echo "$P2_SERIAL[$INDEX] + 1" | bc)
       fi
     fi
   done
@@ -74,17 +82,21 @@ do
     if [ "$temp" != "" ]; then
       N=$(head -n 1 $results | wc -w)
       INDEX=$(head -n 1 $results | awk -v N=$N '{print $N}')
-      SERIAL_P3[$INDEX]=0
-      P3_SERIAL[$INDEX]=0
+      if [ "$SERIAL_P3[$INDEX]" -eq "" ]; then
+        SERIAL_P3[$INDEX]=0
+      fi
+      if [ "$P3_SERIAL[$INDEX]" -eq "" ]; then
+        P3_SERIAL[$INDEX]=0
+      fi
       if [ $INDEX -gt $MAX_NODES ]; then
         MAX_NODES=$INDEX
       fi
       N=$(head -n 6 $results | tail -n 1 | wc -w)
       PARALLEL3COST[$INDEX]=$(head -n 6 $results | tail -n 1 | awk -v N=$N '{print $N}')
       if [ "$PARALLEL3COST[$INDEX]" -gt "$SERIALCOST" ]; then
-        SERIAL_P3[$INDEX]=$SERIAL_P3[$INDEX] + 1
+        SERIAL_P3[$INDEX]=$(echo "$SERIAL_P3[$INDEX] + 1" | bc)
       elif [ "$PARALLEL2COST[$INDEX]" -lt "$SERIALCOST" ]; then
-        P3_SERIAL[$INDEX]=$P3_SERIAL[$INDEX] + 1
+        P3_SERIAL[$INDEX]=$(echo "$P3_SERIAL[$INDEX] + 1" | bc)
       fi
     fi
   done
