a17bc32a68b2a545c7657b4a35f663096cb2575d
Marc Schaer <marc110191@gmail.com>
2015-12-28 16:43:49 +0100

    add terminationCondition for testing purpose

--- parallel3/mpi_ant_colony.cpp
+++ parallel3/mpi_ant_colony.cpp
@@ -291,7 +291,7 @@ int main(int argc, char* argv[]) {
 
   long antsBestCost = INFTY;
 
-  while (external_loop_counter < externalIterations) { //&& terminationCondition < (long) ceilf(externalIterations * onNodeIteration * terminationConditionPercentage)) {
+  while (external_loop_counter < externalIterations && terminationCondition < (long) ceilf(externalIterations * onNodeIteration * terminationConditionPercentage)) {
 
     loop_counter = 0;
     while (loop_counter < onNodeIteration) {
